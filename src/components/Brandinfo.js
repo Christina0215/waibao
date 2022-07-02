b1f7901b3a0a1380c6c9a9304ac76c82a1e55861
Christina0215 <1532706870@qq.com>
2022-07-02 20:51:52 +0800

    添加了图片文件夹

---
+++ src/components/Brandinfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import abroadback from '../assets/abroadback.png';
+
+function Brandinfo(){
+    return(
+        <div>
+            <img src={abroadback} />
+        </div>
+    )
+}
+export default Brandinfo;
