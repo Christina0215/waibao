4863f31bec259d5054698cdefab283a1202e0860
Christina0215 <1532706870@qq.com>
2022-07-03 09:50:00 +0800

    预约界面完成

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import About from './components/About'
 import Brandinfo from './components/Brandinfo'
 import Home from './components/Home'
 import Newbook from './components/Newbook'
+import Booking from './components/booking'
 
 export default function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/about" element={<About />}/>
-          <Route path="/newbook" element={<Newbook />}/>
-          <Route path="/brandinfo" element={<Brandinfo />}/>
-          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />} />
+          <Route path="/newbook" element={<Newbook />} />
+          <Route path="/brandinfo" element={<Brandinfo />} />
+          <Route path='/booking' element={<Booking />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
