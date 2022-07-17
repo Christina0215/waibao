88a63ba63e7ec1ffd27a84de3a9539c2ba3d5f6a
chaihaojian <chaihaojian@gmail.com>
2022-07-17 22:27:40 +0800

    补充页脚

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -15,12 +15,16 @@ const Footer = () => {
             <div className='line'></div>
             <div className='right'>
                 <div className='part part1'>
-                    <span>地址：</span>
-                    <span>邮箱：</span>
-                    <span>睿智源教育·英国</span>
+                    <span>电话：+86 183 2712 3175</span>
+                    <span>邮箱：info@wisdomspringedu.com</span>
+                    <a href='https://www.wisdomspringedu.com/'>
+                        <span id='part1-link'>睿智源教育·英国</span>
+                    </a>                    
                 </div>
                 <div className='part part2'>
-                    <span>ACED™️爱思读™️英国升学体系及测评</span>
+                    <Link to='/admissionsystem'>
+                        <span>ACED™️爱思读™️英国升学体系及测评</span>
+                    </Link>                    
                     <Link to='/consult/download'>
                         <span>留学资料下载</span>
                     </Link>
